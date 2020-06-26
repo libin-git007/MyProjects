2d596af1e3f1b59b559c5c8c38e396394d20ba07
libin <bin.li02@hand-china.com>
2020-06-26 16:34:14 +0800

    delete

--- src/main/java/com/li/org/controller/LoginController.java
+++
@@ -1,9 +0,0 @@
-package com.li.org.controller;
-
-//@RestController
-public class LoginController {
-   // @RequestMapping(value = "login",method= RequestMethod.GET)
-    public String login(){
-        return "please login";
-    }
-}
